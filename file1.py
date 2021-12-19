15430110010a2b437b036043ee51affe0768546c
chewableanimal <chewableanimal@gmail.com>
2021-12-19 03:50:46 -0800

    Added display board functionality

--- file1.py
+++ file1.py
@@ -1,3 +1,5 @@
+import os
+
 #Select Game Mode
 def gameMode():
     try:
@@ -12,14 +14,39 @@ def gameMode():
         print("\nPlease enter only numeric values.")
         return gameMode()
 
+
+def displayBoard(board):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if j < 2:
+                print(board[i][j] + " |", end= " ")
+            else:
+                print(board[i][j], end= " ")
+        if i < 2:
+            print("\n----------")
+    print("\n")
+        
+
+def updateTurn(turn):
+    if turn == True:
+        turn = False
+    else: 
+        turn = True
+
+
 print("Welcome to Tic Tac Toe!\n")
 mode = gameMode()
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+
+os.system('cls' if os.name == "nt" else 'clear')
 
 if mode == 1:
-    print("Singleplayer")
+    displayBoard(board)
 
 elif mode == 2:
-    print("Multiplayer")
+    displayBoard(board)
 
 else:
     quit()
