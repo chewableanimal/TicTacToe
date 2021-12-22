1451c81c44f09fe591164d01ee5b6cce8d3283ff
chewableanimal <chewableanimal@gmail.com>
2021-12-22 01:41:47 -0800

    Added input validation and board update functionality

--- file1.py
+++ file1.py
@@ -15,7 +15,8 @@ def gameMode():
         return gameMode()
 
 
-def displayBoard(board):
+def displayBoard():
+    global board
     for i in range(len(board)):
         for j in range(len(board[i])):
             if j < 2:
@@ -25,7 +26,52 @@ def displayBoard(board):
         if i < 2:
             print("\n----------")
     print("\n")
-        
+
+#Get input function
+def getInput():
+
+    displayBoard()
+    #Get row input
+    while True:
+        try:
+            row = int(input("Please enter input for row(choose from 1-3): "))
+            if row in range(1,4):
+                break
+        except ValueError:
+            print("\nPlease enter only numeric values.")
+            
+    #Get column input
+    while True:
+        try:
+            col = int(input("Please enter input for column(choose from 1-3): "))
+            if col in range(1,4):
+                break
+        except ValueError:
+            print("\nPlease enter only numeric values.")
+
+    row-= 1
+    col-= 1
+
+    if checkBoard(row, col) == False:
+        print("Sorry that area has been filled in, please pick an empty spot on the board.")
+        return getInput()
+
+    return row, col
+    
+
+#Create check board function
+def checkBoard(row, col):
+    global board
+    if board[row][col] != " ":
+        return False
+    return True
+
+def updateBoard(row, col, turn, board):
+    if turn == True:
+        board[row][col] = "X"
+    else:
+        board[row][col] = "O"
+
 
 def updateTurn(turn):
     if turn == True:
@@ -33,20 +79,32 @@ def updateTurn(turn):
     else: 
         turn = True
 
+    return turn
+
 
 print("Welcome to Tic Tac Toe!\n")
 mode = gameMode()
-board = [[" ", " ", " "],
+board = [["X", " ", " "],
          [" ", " ", " "],
          [" ", " ", " "]]
+turn = True
 
 os.system('cls' if os.name == "nt" else 'clear')
 
+#Singleplayer
 if mode == 1:
     displayBoard(board)
 
+#Multiplayer
 elif mode == 2:
-    displayBoard(board)
+    #Place holder before game over functionality is added
+    while True:
+        row, col = getInput()
+        updateBoard(row, col, turn, board)
+        turn = updateTurn(turn)
+            
+
+    
 
 else:
     quit()
